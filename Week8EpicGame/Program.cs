df3a1854a60213889815f18bf4de903a26c03a78
Karl Kryynvald <karl.gustav.krnvald@tktk.ee>
2024-03-05 16:46:49 +0200

    add Week8EpicGame code

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,89 @@
+﻿
+string folderPath = @"C:\data";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+string[] weapon = { "Glamdring", "Gurthang", "Dramborleg", "Ringil", "Andúril" };
+string[] Vweapon = { "Balrogs whip", "Gothmog’s Axe", "Grond", "The One Ring" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+int heroHP = GetCharacterHP(hero);
+int heroStrikeStrength = heroHP;
+Console.WriteLine($"Today {hero} ({heroHP} HP) with {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(Vweapon);
+int villainHP = GetCharacterHP(villain);  
+int villainStrikeStrength = villainHP;
+Console.WriteLine($"Today {villain} ({villainHP} HP) with {villainWeapon} tries to take over Middle-Earth!");
+
+while(heroHP > 0 && villainHP > 0)
+{
+    heroHP = heroHP - Hit(villain, villainStrikeStrength);
+    villainHP = villainHP - Hit(hero, heroStrikeStrength);
+}
+
+Console.WriteLine($"Hero {hero} HP: {heroHP}");
+Console.WriteLine($"Hero {villain} HP: {villainHP}");
+
+if (heroHP > 0)
+{
+    Console.WriteLine($"{hero} saves the day!");
+}
+else if(villainHP > 0)
+{
+    Console.WriteLine($"Darkness wins!");
+}
+else
+{
+    Console.WriteLine("Draw!");
+}
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
+
+static int GetCharacterHP(string characterName)
+{
+    if(characterName.Length < 10)
+    {
+        return 10;
+    }
+    else
+    {
+        return characterName.Length;
+    }
+}
+
+
+static int Hit(string characterName, int characterHP)
+{
+    Random rnd = new Random();
+    int strike = rnd.Next(0, characterHP);
+
+    if(strike == 0)
+    {
+        Console.WriteLine($"{characterName} missed the target!");
+    }
+    else if(strike == characterHP - 1)
+    {
+        Console.WriteLine($"{characterName} made a critical hit!");
+    }
+    else
+    {
+        Console.WriteLine($"{characterName} hit {strike}!");
+    }
+    return strike;
+}
+
+
+
+
